6bacef25837c7629ee03be734e30f40e37b1913a
xucaimao <xcmgj@163.com>
2017-08-31 14:44:49 +0800

    edit at 20170831

--- reversi/reversi20170827.py
+++ reversi/reversi20170827.py
@@ -1,21 +1,24 @@
 #黑白棋
 #根据书上程序，结合苹果手机里的一款黑白棋游戏，进行修改
+#采用面向对象的方法编程
 
 import pygame,sys,time
 
 class Reversi:
-    def __init__(self):
-        self.start_piece=-1        # 初始棋子，即谁先走
-        self.board=[]              # 棋盘(二维列表)
-        self.currten_piece=-1      # 当前下棋的棋子颜色
-        self.enabalemove=False     # 当前棋子有没有合法的落子点，如果没有就需要通过成绩比较判断输赢
-        self.pointlist=[]          # 如果有合法落子点，则记录所有的位置坐标的列表(二维列表)
-        self.reverslist=[]         # 每个合法落子所对应的可翻转的对方棋子的坐标列表(二维列表)
-        self.round=0               # 第几局比赛
-        self.roundscore=[0,0]      # 大比分
-        self.score = [0,0]         # 当前局比分(黑比白)
-        self.step=0                # 当前局步数
-        self.record=[]             # 棋谱记录.undo可以通过棋谱从第一步开始计算的形式，以计算时间换取存储空间(二维列表)
+    # 棋盘(二维列表)。-1黑棋，0空格，1白棋，>100则表示该位置是一种合理的落子点，
+    # 其值减去100所得结果，表示在改点落子后，可以翻转的对方棋子的数量
+    def __init__(self,stpiece):
+        self.start_piece=stpiece        # 初始棋子，即谁先走
+        self.board=[]                   # 棋盘(二维列表)。-1黑棋，0空格，1白棋，>=100则表示改位置是一种合理的走棋点，
+        self.currten_piece=stpiece      # 当前下棋的棋子颜色
+        self.enabalemove=False          # 当前棋子有没有合法的落子点，如果没有就需要通过成绩比较判断输赢
+        self.pointlist=[]               # 如果有合法落子点，则记录所有的位置坐标的列表(二维列表)
+        self.reverslist=[]              # 每个合法落子所对应的可翻转的对方棋子的坐标列表(二维列表)
+        self.round=0                    # 第几局比赛
+        self.roundscore=[0,0]           # 大比分
+        self.score = [0,0]              # 当前局比分(黑比白)
+        self.step=0                     # 当前局步数
+        self.record=[]                  # 棋谱记录.undo可以通过棋谱从第一步开始计算的形式，以计算时间换取存储空间(二维列表)
 
         self.resetBoard()
 
@@ -36,6 +39,7 @@ class Reversi:
         self.board[4][3] = 1
         self.board[4][4] = -1
 
+        self.currten_piece = self.start_piece
         self.roundscore = 0,0
         self.score =0,0
         self.step = 0
@@ -90,6 +94,7 @@ class Reversi:
         # 判断当前棋子current_piece的所有可能的落子点
         # 落子点坐标存在pointlist列表中
         # 每个落子点对应的可以翻转的对方棋子的坐标存在reverslist列表中
+        #如果没有合法落子点(pointlist列表为空)，则返回False。否则返回True
         self.clearlist(self.pointlist)
         self.clearlist(self.reverslist)
 
@@ -101,7 +106,7 @@ class Reversi:
         # 开始遍历所有空白棋格
         for r in range(8):
             for c in range(8):
-                if self.board[r][c] == 0 or self.board[r][c]>=100:
+                if self.board[r][c] == 0 or self.board[r][c] >= 100:
                     revlst = self.judge_point([r, c])
                     if revlst == False:
                         # 该位置不能落子，开始下一个for循环
@@ -111,6 +116,13 @@ class Reversi:
                         self.board[r][c] = 100 + len(revlst)
                         self.pointlist.append([r,c])
                         self.reverslist.append(revlst)
+        #设置当前棋子有无合法落子点的标志
+        if len(self.pointlist) == 0:
+            self.enabalemove = False    #此语句及变量应该可以取消，后期调试看再优化
+            return False
+        else:
+            self.enabalemove = True     #此语句及变量应该可以取消，后期调试看再优化
+            return True
 
     def computermove(self):
         #根据judgeall函数的结果，查找最优落子点的位置
@@ -157,11 +169,90 @@ class Reversi:
             p=self.pointlist.index(pos)                 #取得当前位置在合法落子点列表中的序号
             for r,c in self.reverslist[p]:              #取得翻转棋子的位置，开始翻转棋子
                 self.board[r][c]=self.currten_piece
+            self.getScore()                             #落子后重新计算分数
             self.currten_piece=-self.currten_piece      #落子后改变棋子颜色
-            self.judge_allpoint()                       #预先判断所有棋子的可能走法
+            self.judge_allpoint()                       #预先判断下一步的所有可能走法
 
 
 
+#function of main
+
+#这些全局数据都是屏幕显示相关的，最好改成一个列表。将此列表作为参数，传入显示函数中
+global boardsize,boardtopleft,boardrightbottom
+global gsize  #棋盘格子的尺寸
+global buttontop,buttonbottom
+global buttonleft,buttonright
+global bsize     #按钮尺寸
+boardsize=550
+boardtopleft=22
+boardrightbottom=572
+gsize=68.75  #棋盘格子的尺寸
+buttontop=180
+buttonbottom=204
+buttonleft=628
+buttonright=876
+bsize=62     #按钮尺寸
+
+
+
+def drawscreen(win,reversi,surflist,bpos):
+    # 绘制棋盘,win为棋盘所在窗口，bpos为button的坐标[row,col]，此处只是为了测试程序，此参数可以删除
+    # surflist为背景图黑棋行，背景图白棋行，黑棋，白棋和提示棋的Surface组成的列表
+    #本程序用了一些屏幕坐标，后期可以改成列表将这些数据传入
+
+    #根据当前棋子更换背景图片
+    if reversi.currten_piece == -1:
+        win.blit(surflist[0], [0, 0])
+    else:
+        win.blit(surflist[1], [0, 0])
+    #显示当前局的比分
+    font1 = pygame.font.Font(None, 32)
+
+    str1 = str(bpos[0]) + " , " + str(bpos[1])
+    scorestr = str(reversi.score[0]) + " : " + str(reversi.score[1])
+    txt1_surf = font1.render(str1, 1, (0, 0, 0))
+    score_surf = font1.render(scorestr, 1, (94,39,7))
+    #txt3_surf = font1.render(str3, 1, (0, 0, 255))
+    win.blit(txt1_surf, [640, 250])
+    win.blit(score_surf, [730,36])
+    #win.blit(txt3_surf, [605, 450])
+
+    #开始显示整个棋盘
+
+    for r in range(8):
+        for c in range(8):
+            if reversi.board[r][c] == -1:
+                #黑棋
+                win.blit(surflist[2], [c * gsize  + boardtopleft, r * gsize + boardtopleft])
+            elif reversi.board[r][c] == 1:
+                #白棋
+                win.blit(surflist[3], [c * gsize  + boardtopleft, r * gsize + boardtopleft])
+            elif reversi.board[r][c] >= 100:
+                #提示棋
+                win.blit(surflist[4], [c * gsize  + boardtopleft, r * gsize + boardtopleft])
+                str2=str(reversi.board[r][c]-100)
+                txt2_surf = font1.render(str2, 1, (0, 0, 0))
+                win.blit(txt2_surf, [c * gsize + boardtopleft+25, r * gsize + boardtopleft+25])
+
+
+    pygame.display.flip()
+
+
+
+def cursor2button(x,y):
+    #屏幕光标位置转换为按钮
+    #[0,0]~[7,7]为棋盘相应格子
+    #[10,0]~[10,3]为四个控制按钮
+    #[-1,-1]为不合法的点击
+    row=-1
+    col=-1
+    if x>=boardtopleft and x<=boardrightbottom and y>=boardtopleft and y<=boardrightbottom:
+        row = int((y - boardtopleft) / gsize)
+        col = int((x - boardtopleft) / gsize)
+    elif x>=buttonleft and x<=buttonright and y>=buttontop and y<=buttonbottom:
+        row=10
+        col=int((x-buttonleft)/bsize)
+    return row,col
 
 
 
@@ -171,6 +262,7 @@ class Reversi:
 #main
 pygame.init()
 screen=pygame.display.set_mode([900,600])
+#初始化用于显示的各种图片
 surflist=[]
 surflist.append(pygame.image.load("qipan9x6_b.png"))
 surflist.append(pygame.image.load("qipan9x6_w.png"))
@@ -178,18 +270,64 @@ surflist.append(pygame.image.load("qizi_black.png"))
 surflist.append(pygame.image.load("qizi_white.png"))
 surflist.append(pygame.image.load("qizi_tishi.png"))
 
-#初始化为人人对战
-player2="computer"
+#初始化由于显示的各个坐标
+poslist=[]
 
-#-1为黑棋，1为白棋，0无子，
-# 大于100，说明该处落子合法，其值减去100就为该处落子后，可以翻转的对方棋子的数量
-#piece为当前棋子的颜色
-piece=-1
+
+
+#firstpiece为先手棋子的颜色。此处可以扩展选择先手棋子的颜色
+firstpiece=-1
 
 #生成棋盘的实例
-myreversi=Reversi()
+myreversi=Reversi(firstpiece)
 
-drawscreen(screen,myboard,surflist,[-1,-1],piece)
+drawscreen(screen,myreversi,surflist,[-1,-1])
+
+
+while True:
+    for event in pygame.event.get():
+        #开始等待玩家落子或选择按钮
+        if event.type == pygame.QUIT:
+            pygame.display.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            cursor_x=event.pos[0]                 #取得x坐标
+            cursor_y=event.pos[1]
+            row,col=cursor2button(cursor_x,cursor_y)
+            #刷新屏幕
+            drawscreen(screen,myreversi,surflist,[row,col])
+            if row==-1 and col ==-1:
+                #非法点击，提示音，进入下一次for循环
+                continue
+            if row==10 and col==0:
+                #离开
+                pygame.display.quit()
+                print("Player select quit game")
+                sys.exit()
+            elif row==10 and col==1:
+                # 悔棋
+                if len(myreversi.record)<=1:
+                    continue
+
+                pass
+
+
+            elif row==10 and col==2:
+                # 认输
+                pass
+            elif row==10 and col==3:
+                # 重玩
+                myreversi.resetBoard()
+
+                drawscreen(screen, myreversi, surflist, [-1, -1])
+                continue
+            else:
+                #棋盘上的点击
+                myreversi.addpiece([row,col])
+                drawscreen(screen, myreversi, surflist, [row, col])
+
+
+
 
 
 
